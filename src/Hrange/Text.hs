992349bc539949a87c0cc065b2136e14c27e7c0e
Xavier Shay <xavier@squareup.com>
2016-04-01 18:35:46 -0700

    Better name for charIsSame

--- src/Hrange/Text.hs
+++ src/Hrange/Text.hs
@@ -26,13 +26,13 @@ match (ShowableRegex _ rx) = matchTest rx . T.unpack
 
 commonSuffix :: [String] -> String
 commonSuffix xs =
-  reverse . map head . takeWhile charIsSame . transpose . map reverse $ xs
+  reverse . map head . takeWhile elemsAreSame . transpose . map reverse $ xs
   where
     l = length xs - 1
 
-    charIsSame :: String -> Bool
-    charIsSame []     = False
-    charIsSame (c:cs) = length cs == l && all (== c) cs
+    elemsAreSame :: Eq a => [a] -> Bool
+    elemsAreSame []     = False
+    elemsAreSame (c:cs) = length cs == l && all (== c) cs
 
 -- http://www.rosettacode.org/wiki/Strip_a_set_of_characters_from_a_string#Haskell
 stripChars :: String -> String -> String
